0e97517b0b9b5ceb3f6e4cb1b0901b64629d42f6
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-04-27 11:20:53 -0700

    Add map panning to new marker location and removal of marker infowindow when another marker is clicked

--- js/main.js
+++ js/main.js
@@ -1,40 +1,44 @@
 var markers = [];
 var map;
+var infoWindow;
 
 var initMap = function() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.7749, lng: -122.4194},
+    center: { lat: 37.7749, lng: -122.4194 },
     zoom: 13
   });
+  infoWindow = new google.maps.InfoWindow();
 }
 
 $(document).ready(function() {
-  
-  var addMarker = function() {
-    var input = $("#message-form").serializeArray();
-    console.log(input);
-    var latitude = parseFloat(input[0].value);
-    var longitude = parseFloat(input[1].value);
-    var marker = new google.maps.Marker({
-      position: {lat: latitude, lng: longitude},
-      map: map,
-      title: input[2].value
-    });
-    markers.push(marker);
-    addInfoWindow(marker);
-  };
-
-  var addInfoWindow = function(marker) {
-    var infoWindow = new google.maps.InfoWindow();
-    infoWindow.marker = marker;
-    marker.addListener("click", function() {
-      infoWindow.setContent("<div class='marker-info'>" + marker.title + "</div");
-      infoWindow.open(map, marker);
-    });
-  };
 
   $("#message-form").submit(function(event) {
     event.preventDefault();
     addMarker();
   });
+
+  var addMarker = function() {
+    var input = $("#message-form").serializeArray();
+    var position = { lat: parseFloat(input[0].value), 
+                     lng: parseFloat(input[1].value) };
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: input[2].value
+    });
+    markers.push(marker);
+    addInfoWindow(marker);
+    map.panTo(position);
+  };
+
+  var addInfoWindow = function(marker) {
+    marker.addListener("click", function() {
+      if (infoWindow.marker != marker) {
+        infoWindow.marker = marker;
+        infoWindow.setContent("<div class='marker-info'>" + marker.title + "</div");
+        infoWindow.open(map, marker);
+      }
+    });
+  };
+
 });
